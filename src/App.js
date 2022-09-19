1b10744045e63172a27bb9e85e2c37f557d5fa91
nsmlenceva <nsmlenceva@rambler.ru>
2022-09-19 10:10:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from 'react';
+
 
 function App() {
+  const [present, setPresent] = useState(0);
+  const {id, name, description, image} = data[present];
+
+  const PreviousPr = ()  => {
+    setPresent( present => {
+      present --;
+      if (present < 0 ) {present = data.length-1}
+      return present;
+    }
+    )
+  }
+
+  const NextPr = () => {
+    setPresent( present => {
+      present ++;
+      if (present > data.length-1) {present = 0}
+      return present;
+    }
+    )
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>{id} - {name}</h1>
+      </div>
+      <div className='container'>
+        <img src={image} alt="present" width="300px"/>
+      </div>
+      <div className='container'>
+        <p>{description}</p>
+      </div>
+      <div className='container'>
+        <button onClick={PreviousPr}>Previous</button>
+        <button onClick={NextPr}>Next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const data = [
+    {
+        id: 1,
+        name: "Смартфон",
+        description: "Смартфон — самое часто используемое устройство у любого человека. Новости, общение, работа — все эти процессы сконцентрированы именно в мобильном устройстве. Такой подарок на новый год — это гарантия того, что он пригодится, а человек получит положительные эмоции.",
+        image: "https://img.freepik.com/free-photo/high-angle-of-smartphone-with-duct-tape_23-2149404217.jpg?w=360&t=st=1662993528~exp=1662994128~hmac=449dd10e932196f10a938705751c18dc0c0788b396f83c704bcca433f7821f42"
+    },
+    {
+        id: 2,
+        name: "Настольная игра",
+        description: "Выбрав в качестве подарка настольную игру, вы подарите гораздо больше - много-много часов радости, удовольствия, домашнего уюта, когда вся семья вместе и всем от этого хорошо :) Положите для своих близких под ёлочку одну из красивых и увлекательных семейных игр.",
+        image: "https://img.freepik.com/free-vector/board-game-collection-isometric-design_23-2148702469.jpg?size=626&ext=jpg&ga=GA1.2.12781574.1649802887"
+    },
+    {
+        id: 3,
+        name: "Билеты на каток",
+        description: "Подарите близким путешествие в мир зимней грации на льду - сертификат на билет на каток.",
+        image: "https://img.freepik.com/free-vector/isometric-generation-family-horizontal-banner-with-parents-and-kids-skating-on-ice-with-button-and-text_1284-54978.jpg?size=626&ext=jpg&ga=GA1.2.12781574.1649802887"
+    },
+    {
+        id: 4,
+        name: "Книга",
+        description: "Как бы банально это ни звучало, но, как показывает практика, книга — лучший подарок.",
+        image: "https://img.freepik.com/free-photo/front-view-composition-with-different-books_23-2148851051.jpg?size=626&ext=jpg&ga=GA1.2.12781574.1649802887"
+    },
+    {
+        id: 5,
+        name: "Планшет",
+        description: "",
+        image: "https://img.freepik.com/free-psd/minimalist-device-mockup-in-real-context_23-2149067470.jpg?size=626&ext=jpg&ga=GA1.2.12781574.1649802887"
+    },
+    {
+        id: 6,
+        name: "Набор для творчества",
+        description: "Набор для творчества станет замечательным подарком к Новому году!",
+        image: "https://img.freepik.com/free-vector/different-art-supplies-vector-illustrations-set-tools-and-equipment-for-painting-and-drawing-paint-tubes-brushes-pencils-watercolor-isolated-on-white-background-art-craft-creativity-concept_74855-21781.jpg?size=626&ext=jpg&ga=GA1.2.12781574.1649802887"
+    },
+    {
+        id: 7,
+        name: "Ноутбук",
+        description: "Близится Новый год, и если вы до сих пор не выбрали подарки – самое время присмотреться к действительно универсальным и полезным устройствам. В год, когда чуть ли не весь мир перешел на удаленную работу, хорошим выбором станут современные ноутбуки.",
+        image: "https://img.freepik.com/free-psd/work-station-table-business-technology_53876-57335.jpg?size=626&ext=jpg&ga=GA1.2.12781574.1649802887"
+    },
+    {
+        id: 8,
+        name: "Ювелирные изделия",
+        description: "Подарок на Новый год ждут все, вне зависимости от возраста. Правда, детям проще мечтать. Мечтам взрослых подрезают крылья напоминания о бюджете, ипотеке и прочих скучных вещах. Впрочем, тем приятнее найти под ёлкой настоящие, сверкающие как шампанское в бокале, неожиданные или долгожданные драгоценности! Не сдерживайте порыва благодарности близким – дарите драгоценности!",
+        image: "https://img.freepik.com/free-vector/jewelry-accessories-realistic-set_1284-16770.jpg?size=338&ext=jpg&ga=GA1.2.12781574.1649802887"
+    },
+    {
+        id: 9,
+        name: "Сладкий подарок",
+        description: "Новый год — семейный праздник родом из детства. Поэтому малыши особенно ждут наступления этой волшебной ночи, чтобы утром под елкой обнаружить кучу подарков. Среди них обязательно должны быть конфеты, печенья и прочие сладости.",
+        image: "https://img.freepik.com/free-photo/chocolate-and-spices-isolated-on-white_2829-15566.jpg?size=626&ext=jpg&ga=GA1.2.12781574.1649802887"
+    },
+    {
+        id: 10,
+        name: "Мягкая игрушка",
+        description: "Игрушки – прекрасные новогодние подарки, ведь самое главное в этот праздник – это порадовать особенным сказочным настроением, а чудесные мягкие игрушки возвращают нас в атмосферу детства!",
+        image: "https://img.freepik.com/free-photo/front-view-of-cute-little-baby-boy-cupcake-and-teddybear_23-2148415544.jpg?size=338&ext=jpg&ga=GA1.2.12781574.1649802887"
+    }
+]
